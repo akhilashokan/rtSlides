92a4fa01584d852b69b061cbb96e73210120b6f0
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-10-17 14:49:53 +0530

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '56a7ae1528fe05fb3243');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7eb26439ac8733920e38');
