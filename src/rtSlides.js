55b4e00e6bcceb8faf77b37c7e11a8dff1cdc087
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-10-19 13:31:42 +0530

    +keyboard nav +cleanup

--- src/rtSlides.js
+++ src/rtSlides.js
@@ -4,21 +4,40 @@ document.addEventListener('readystatechange', () => {
     }
 })
 
+
 function load() {
-    let slides = document.querySelector('.rt_slides')
-    let wrapper = document.querySelector('.rt_wrapper')
-    let width = wrapper.getBoundingClientRect().width
-    let nav = slides.querySelector('.rt_navigation')
-    var xPos = 0
-    let max = (wrapper.childElementCount - 1) * width
+    const slides = document.querySelector('.rt_slides')
+    const wrapper = document.querySelector('.rt_wrapper')
+    const width = wrapper.getBoundingClientRect().width
+    const max = (wrapper.childElementCount - 1) * width
+    rtSlideNav(slides, wrapper, width, max)
+    keyListener(wrapper, width, max)
+    // touchListner()
+}
+var xPos = 0
+
+function rtSlideNav(slidesContainer, wrapper, width, max) {
+    let nav = slidesContainer.querySelector('.rt_navigation')
     nav.addEventListener('click', (e) => {
-        console.log(xPos, max);
-        if (e.target.accessKey === 'right' && xPos < max) {
-            xPos += width
-        }
-        if (e.target.accessKey === 'left' && xPos >= 0) {
-            xPos -= width
-        }
+        if (e.target.accessKey === 'right' && xPos < max) { xPos += width }
+        if (e.target.accessKey === 'left' && xPos >= 0) { xPos -= width }
         wrapper.style.transform = `translateX(-${xPos}px)`
     })
 }
+
+function keyListener(wrapper, width, max) {
+    document.addEventListener('keydown', ({ key }) => {
+        if (key === 'ArrowRight' && xPos < max) { xPos += width }
+        if (key === 'ArrowLeft' && xPos >= 0) { xPos -= width }
+        wrapper.style.transform = `translateX(-${xPos}px)`
+    })
+}
+
+
+// function touchListner() {
+//     wrapper.addEventListener("touchmove", (e) => {
+//         let xPos = e.touches[0].clientX
+//         console.log(e.touches[0].clientX);
+//         wrapper.style.transform = `translateX(${xPos}px)`
+//     }, { passive: true })
+// }
